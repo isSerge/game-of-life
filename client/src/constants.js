aa52ab6fe177665c0e12a922b7225a18d6d6e97c
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2019-01-20 22:59:53 +0200

    added constants

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,10 +1,19 @@
-export default {
+export const topics = {
     INITIAL_REQUEST: 'initial-request',
     INITIAL_RESPONSE: 'initial-response',
     PLACE_CELL: 'place-cell',
+    PLACE_PATTERN: 'place-pattern',
     START_TICKS: 'start-ticks',
     NEXT_TICK: 'next-tick',
     PAUSE_TICK: 'pause-tick',
     REFRESH_TICKS: 'refresh-ticks',
     WORLD_UPDATE: 'world-update',
+}
+
+export const patternNames = {
+    TUB: 'Tub',
+    BOAT: 'Boat',
+    BLINKER: 'Blinker',
+    GLIDER: 'Glider',
+    DEFAULT: 'Default',
 }
--- server/constants.js
+++ server/constants.js
@@ -1,10 +1,24 @@
-module.exports = {
+const patternNames = {
+    TUB: 'Tub',
+    BOAT: 'Boat',
+    BLINKER: 'Blinker',
+    GLIDER: 'Glider',
+    DEFAULT: 'Default',
+}
+
+const topics = {
     INITIAL_REQUEST: 'initial-request',
     INITIAL_RESPONSE: 'initial-response',
     PLACE_CELL: 'place-cell',
+    PLACE_PATTERN: 'place-pattern',
     START_TICKS: 'start-ticks',
     NEXT_TICK: 'next-tick',
     PAUSE_TICK: 'pause-tick',
     REFRESH_TICKS: 'refresh-ticks',
     WORLD_UPDATE: 'world-update',
+}
+
+module.exports = {
+    patternNames,
+    topics,
 }
