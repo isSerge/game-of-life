2579592cced7d523c9b6aa721c160a834b552176
Serge Kovbasiuk <sergekovbasiuk@Serges-MacBook-Pro.local>
2019-01-20 19:09:06 +0200

    added dominant color

--- server/board.js
+++ server/board.js
@@ -1,11 +1,20 @@
-// const { floor, random } = Math
-// const fillWithRandomData = grid =>
-//   grid.map(x => x.map(_ => floor(random() * 2)))
+const { mode } = require('./utils')
+const { floor, random } = Math
+
+const fillWithRandomData = grid =>
+    grid.map(x =>
+        x.map(() => {
+            if (floor(random() * 2)) {
+                return '#000'
+            }
+            return 0
+        }),
+    )
 
 const SIZE = 20
 
 // create 2-dimensional array
-const createGrid = size => new Array(size).fill(0).map(_ => new Array(size).fill(0))
+const createGrid = size => new Array(size).fill(0).map(() => new Array(size).fill(0))
 
 const isWithinBounds = v => v >= 0 && v < SIZE
 const areWithinBounds = (x, y) => isWithinBounds(x) && isWithinBounds(y)
@@ -22,36 +31,29 @@ const getNeighbourCoordinates = (x, y) =>
         [x + 1, y + 1],
     ].filter(xyArr => areWithinBounds(...xyArr))
 
-const countNeighbours = (grid, xIndex, yIndex) => {
-    const neighbours = getNeighbourCoordinates(xIndex, yIndex)
-    return neighbours.reduce((acc, [x, y]) => {
-        if (grid[x][y]) {
-            return acc + 1
-        }
-    }, 0)
+const countNeighbours = (grid, rowIndex, colIndex) => {
+    const neighbourCoordinates = getNeighbourCoordinates(rowIndex, colIndex)
+    const neighbours = neighbourCoordinates.map(([x, y]) => grid[x][y])
+    const dominantColor = mode(neighbours)
+    const sum = neighbours.filter(x => x).length
+    return [sum, dominantColor]
 }
 
 const getNextGeneration = grid =>
     grid.map((x, xIndex) =>
         x.map((_, yIndex) => {
             const cell = grid[xIndex][yIndex]
-            const neighbours = countNeighbours(grid, xIndex, yIndex)
-            // calc dominating color
+            const [neighbours, dominantColor] = countNeighbours(grid, xIndex, yIndex)
 
             if (cell === 0 && neighbours === 3) {
-                return 1
-            } else if (cell === 1 && (neighbours < 2 || neighbours > 3)) {
+                return dominantColor
+            } else if (cell !== 0 && (neighbours < 2 || neighbours > 3)) {
                 return 0
             } else {
                 return cell
             }
         }),
     )
-
-// export const grid =
-//   // fillWithRandomData(
-//   createGrid(SIZE)
-// // )
 
 const putCellOnCoordinates = (cells, x, y, color) =>
     cells.map((row, xIndex) =>
@@ -67,4 +69,5 @@ module.exports = {
     getNextGeneration,
     createGrid,
     putCellOnCoordinates,
+    fillWithRandomData,
 }
--- server/utils.js
+++ server/utils.js
@@ -1,5 +1,21 @@
 const generateRandomColor = () => `#${(((1 << 24) * Math.random()) | 0).toString(16)}`
 
+const mode = arr =>
+    arr.reduce(
+        (current, item) => {
+            if (item) {
+                const val = (current.numMapping[item] = (current.numMapping[item] || 0) + 1)
+                if (val > current.greatestFreq) {
+                    current.greatestFreq = val
+                    current.mode = item
+                }
+            }
+            return current
+        },
+        { mode: null, greatestFreq: -Infinity, numMapping: {} },
+    ).mode
+
 module.exports = {
     generateRandomColor,
+    mode,
 }
